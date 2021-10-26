494e4e57fca1c69a29c14a65c21a0b1429c22417
cedcapistrano27 <cedcapistrano27@gmail.com>
2021-10-27 00:09:54 +0800

    user

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-<html>
-    <head>
-
-    </head>
-    <body>
-    YAMETE KUDASAI !!! AHHH
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    
+</style>
+<body>
+    
+</body>
 </html>
