f64a0018dee3596fd2c6e23bb2622c60e4885728
SenpaiChris09 <christiancullados73@gmail.com>
2021-10-26 23:58:14 +0800

    yamete onii chan

--- index.php
+++ index.php
@@ -3,12 +3,6 @@
 
     </head>
     <body>
-<<<<<<< HEAD
-        <h1>Hello World!</h1>
-=======
-       madara is fuckin god 
->>>>>>> a437dbab99e73fdfc5ffcfc59db059f74cdecd89
-umay!
-
+    YAMETE KUDASAI !!! AHHH
     </body>
 </html>
