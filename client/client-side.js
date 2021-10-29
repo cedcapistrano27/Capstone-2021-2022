16b9151bd7f26f6497b830961e4d4c036444f411
SenpaiChris09 <christiancullados73@gmail.com>
2021-10-29 10:46:05 +0800

    me date na

--- client/client-side.js
+++ client/client-side.js
@@ -9,3 +9,18 @@ modalBtn.addEventListener('click', function(){
 modalClose.addEventListener('click', function(){
     modalBg.classList.remove('bg-active');
 });
+
+
+$(function () {
+    $('#datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        autoclose: true,
+        todayHighlight: true,
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        autoclose: true,
+        changeMonth: true,
+        changeYear: true,
+        orientation: "button"
+    }).datepicker('update', new Date());
+});
