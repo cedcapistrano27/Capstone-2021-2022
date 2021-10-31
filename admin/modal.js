5c6ac1415840ee3a630c88267ed93e347a24d2e9
SenpaiChris09 <christiancullados73@gmail.com>
2021-10-31 10:34:55 +0800

    ok

---
+++ admin/modal.js
@@ -0,0 +1,17 @@
+// Get the modal
+var mod = document.getElementById('id01');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == mod) {
+    mod.style.display = "none";
+  }
+}
+
+
+var modal2 = document.getElementById('id02');
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
