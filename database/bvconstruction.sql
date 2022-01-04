86d6a6948eddfc426a59e9e84023bedb36d17dc7
SenpaiChris09 <christiancullados73@gmail.com>
2022-01-04 14:02:21 +0800

    updated database baby

--- database/bvconstruction.sql
+++ database/bvconstruction.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2021 at 02:37 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
+-- Generation Time: Jan 04, 2022 at 07:01 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,11 +30,26 @@ SET time_zone = "+00:00";
 CREATE TABLE `appointment` (
   `APPID` int(255) NOT NULL,
   `UID` int(255) NOT NULL,
-  `date` date NOT NULL,
-  `a_type` varchar(50) NOT NULL,
+  `date` datetime NOT NULL,
   `a_details` text NOT NULL,
-  `Status` varchar(100) NOT NULL
+  `status` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`APPID`, `UID`, `date`, `a_details`, `status`) VALUES
+(1, 0, '0000-00-00 00:00:00', 'ahhahahahhahahaha hatdog', ''),
+(2, 0, '2021-12-27 00:00:00', 'hatdog talaga', ''),
+(3, 0, '2021-12-30 14:00:00', 'hatdog na hatdog ang usapan', ''),
+(4, 0, '0000-00-00 00:00:00', 'Gujasuuy  als;kdjuiy lorem lorem lored\r\n', 'pending'),
+(5, 0, '2022-01-07 02:35:00', 'hatdog hatdog hatdog hatdog', 'pending'),
+(6, 0, '2022-01-06 14:43:00', 'haha hatdog na bitin', 'pending'),
+(7, 3010, '2022-01-13 13:39:00', 'asfdggg', 'pending'),
+(8, 3022, '2022-01-17 17:40:00', 'hatfdog ajsjhhajdshadpoj ', 'pending'),
+(9, 3022, '2022-01-12 04:00:00', 'adksjhguyoiyu  m;alsfj missyou', 'pending'),
+(10, 3010, '2022-01-16 17:00:00', 'for update of the ongoing project aka Payamansion 5.6', 'pending');
 
 -- --------------------------------------------------------
 
@@ -84,25 +99,30 @@ CREATE TABLE `user` (
   `ID_proof` varchar(255) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `usertype` varchar(255) NOT NULL,
-  `ImageID` text NOT NULL
+  `usertype` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UID`, `fname`, `mname`, `lname`, `address`, `email`, `cnumber`, `ID_proof`, `username`, `password`, `usertype`, `ImageID`) VALUES
-(3005, 'chris', 'g', 'cullados', 'tanzang luma 6', 'gogo@gmail.com', 922278489, 'sss id, umid id, philhealth id, national id', 'xtianpuh', '12345678', 'admin', ''),
-(3007, '', '', '', '', '', 0, '', 'haha', 'xd', 'common', ''),
-(3009, '', '', '', '', 'afdy@gmail.com', 0, '', 'newuser2', '87654321', 'common', ''),
-(3010, 'mang', 'g', 'tomas', 'none', 'maosdu@gmail.com', 102884, 'SSS', 'user3', 'qwerty', 'common', ''),
-(3022, 'asdgeee', 'qwrrrt', 'sddf', 'afdd2', 'bagong@email.com', 456456, '', 'bagonguser', '12345678', 'common', ''),
-(3023, '', '', '', '', 'christiancullados73@gmail.com', 0, '', 'newuser2', '12345678', 'common', '');
+INSERT INTO `user` (`UID`, `fname`, `mname`, `lname`, `address`, `email`, `cnumber`, `ID_proof`, `username`, `password`, `usertype`) VALUES
+(3005, 'chris', 'g', 'cullados', 'tanzang luma 6', 'gogo@gmail.com', 922278489, 'sss id, umid id, philhealth id, national id', 'xtianpuh', '12345678', 'admin'),
+(3007, '', '', '', '', '', 0, '', 'haha', 'xd', 'common'),
+(3009, '', '', '', '', 'afdy@gmail.com', 0, '', 'newuser2', '87654321', 'common'),
+(3010, 'mang', 'L', 'tomas', 'none', 'maosdu@gmail.com', 102884, '', 'user3', 'qwerty', 'common'),
+(3022, 'asdgeee', 'qwrrrt', 'sddf', 'afdd2', 'bagong@email.com', 456456, '', 'bagonguser', '12345678', 'common'),
+(3023, '', '', '', '', 'christiancullados73@gmail.com', 0, '', 'newuser2', '12345678', 'common');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`APPID`);
 
 --
 -- Indexes for table `payment`
@@ -125,6 +145,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `APPID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `payment`
