bcf8f1916f986763b69a6a7f965edd2dffe7ad65
benbendict08 <benbendict08@gmail.com>
2022-01-13 05:30:08 +0800

    ben update

--- appointment_list.php
+++ appointment_list.php
@@ -8,53 +8,13 @@ $Uname = $_SESSION['username'];
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--  
-    Document Titleqwfasdasd
-    =============================================
-    -->
-    <title>BV Contruction</title>
-    <!--  
-    Favicons
-    =============================================
-    -->
-    <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="assets/images/favicons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="stylesheet" href="client.css">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="assets/images/favicons/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-    <!--  
-    Stylesheets
-    =============================================
     
-    -->
-    <!-- Default stylesheets-->
+    <title>BV Contruction</title>
+    
+    <link rel="stylesheet" href="client.css">
+    
     <link href="assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Template specific stylesheets-->
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
-    <link href="assets/lib/animate.css/animate.css" rel="stylesheet">
-    <link href="assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
-    <link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
-    <link href="assets/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="assets/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
-    <link href="assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
-    <link href="assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
-    <!-- Main stylesheet and color file-->
+   
     <link href="assets/css/style.css" rel="stylesheet">
     <link id="color-scheme" href="assets/css/colors/default.css" rel="stylesheet">
   </head>
@@ -83,13 +43,15 @@ $Uname = $_SESSION['username'];
       <div class="container">
         <div class="row">
           <div class="col-sm-2">
-            <img class="col-sm-12" src="images/avatar.png" style="border-radius: 50%;width: 100%;height: auto;">
+          <a href="user_update.php">
+            <img class="col-sm-2" src="images/avatar.png" style="border-radius: 50%;width: 100%;height: auto;cursor:pointer;">
+            </a>
             <h1 class="col-sm-12" style="font-size: 26px;"><?php echo $Uname ?></h1>
             <a class="col-sm-12" href="dashboard.php">Dashboard</a>
             <a class="col-sm-12" href="client.php">Create an Appointment</a>
             <a class="col-sm-12" href="appointment_list.php">Appointment/s</a>
             <a class="col-sm-12" href="projects.php">Project/s</a>
-            <a class="col-sm-12"href="user_update.php">User Profile</a>
+            
             
           </div>
           <div class="col-sm-10">
@@ -180,53 +142,7 @@ $Uname = $_SESSION['username'];
       
       
 
-        <!-- FOOTER -->
-        <div class="bg-dark text-center">
-          <div class="container">
-            
-                  <h5 class="widget-title font-alt">About BV Construction</h5>
-               
-          </div>
-        </div>
-        <hr class="divider-d">
-        <footer class="footer bg-dark">
-          <div class="container">
-            <div class="row text-center">
-              <div class="col-sm-3">
-                <p>Company proect with high quality services and lead modern look interior.</p>
-              </div>
-              <div class="col-sm-3">
-                <p>Phone: +1 234 567 89 10</p> Fax: +1 234 567 89 10
-              </div>
-              <div class="col-sm-3">
-                <p>Email:<a href="#"> somecompany@example.com</a></p>
-              </div>
-              <div class="col-sm-3">
-                <div class="footer-social-links"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-dribbble"></i></a><a href="#"><i class="fa fa-skype"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-      </div>
-      <div class="scroll-up"><a href="#totop"><i class="fa fa-angle-double-up"></i></a></div>
-    </main>
-    <!--  
-    JavaScripts
-    =============================================
-    -->
     <script src="assets/lib/jquery/dist/jquery.js"></script>
-    <script src="assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/lib/wow/dist/wow.js"></script>
-    <script src="assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
-    <script src="assets/lib/isotope/dist/isotope.pkgd.js"></script>
-    <script src="assets/lib/imagesloaded/imagesloaded.pkgd.js"></script>
-    <script src="assets/lib/flexslider/jquery.flexslider.js"></script>
-    <script src="assets/lib/owl.carousel/dist/owl.carousel.min.js"></script>
-    <script src="assets/lib/smoothscroll.js"></script>
-    <script src="assets/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
-    <script src="assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
-    <script src="assets/js/plugins.js"></script>
     <script src="assets/js/main.js"></script>
   </body>
 </html>
--- dashboard.php
+++ dashboard.php
@@ -55,6 +55,44 @@ $row3 = mysqli_fetch_array($project_result3);
     <link href="dashboardstyle.css" rel="stylesheet">
 
   </head>
+  <style>
+    .myappoinment {
+      max-width:100%;
+      background-color:whitesmoke;
+      border-radius: 20px;
+      padding: 10px 10px;
+      box-shadow: 5px 5px 10px grey;
+      text-align: center;
+    }
+    .myappoinment:hover {
+      background-color: rgb(186, 242, 186);
+    }
+    .AL {
+      display:none;
+      position: absolute;
+      margin: 20% 0%;
+      border-radius: 20px;
+      box-shadow: 10px 10px 20px rgb(133, 131, 131);
+      background-color:rgb(248, 245, 245);
+      margin-bottom:25px;
+      width: 900px;
+      height: 500px;
+      
+    }
+    .close {
+      font-size: 50px;
+    }
+  </style>
+  <script>
+    function listclose(){
+      document.getElementById("AL").style.display= "none";
+
+    }
+    function openclose(){
+      document.getElementById("AL").style.display= "block";
+      //document.getElementById("APM").style.backgroundColor="blue";
+    }
+  </script>
   <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60" bgcolor="#0000">
     <main>
       <div class="page-loader">
@@ -77,21 +115,24 @@ $row3 = mysqli_fetch_array($project_result3);
         </div>
       </nav>
       <section class="module module-small">
-      <div class="container" style="margin-top: -20px;">
-        <div class="row">
+      <div class="container"  style="margin-top: -20px;">
+        <div class="row" >
+          
           <div class="col-sm-2" style="margin-top: 20px;">
-            <img class="col-sm-2" src="images/avatar.png" style="border-radius: 50%;width: 100%;height: auto;">
+            <a href="user_update.php">
+            <img class="col-sm-2" src="images/avatar.png" style="border-radius: 50%;width: 100%;height: auto;cursor:pointer;">
+            </a>
             <span>________________________</span>
             <div class="container2 col-sm-10" style="font-size: 14px; padding-bottom: 40px; width:250px; right:30px">
               <a class="col-sm-12" href="dashboard.php">Dashboard</a>
               <a class="col-sm-12" href="client.php">Create an Appointment</a>
               <a class="col-sm-12" href="appointment_list.php">Appointment/s</a>
               <a class="col-sm-12" href="projects.php">Project/s</a>
-              <a class="col-sm-12"href="user_update.php">User Profile</a>
             </div>
            
           </div>
-          <div class="col-sm-10" style="background-color:lightgray;height: 1300px;padding: 0px 35px 0px 35px;">
+          <div id="APM">
+          <div class="col-sm-10" style="background-color:rgb(232, 232, 232);height: 1300px;padding: 0px 35px 0px 35px;">
             <span style="font-size: 30px;color: black;">Dashboard</span>
             <div class="nameheader col-sm-12" style="max-width:100%;background-color:whitesmoke;border-radius: 20px;padding: 10px 10px;box-shadow: 5px 5px 10px grey;">
                 <div class="col-sm-6" style="padding: 0px 0px 0px 0px;">
@@ -115,37 +156,30 @@ $row3 = mysqli_fetch_array($project_result3);
             </div>
             <div class="col-sm-6">
               <span style="font-size: 30px;color: black;">My Appointment</span>
-              <div class="col-sm-12" style="max-width:100%;background-color:whitesmoke;border-radius: 20px;padding: 10px 10px;box-shadow: 5px 5px 10px grey;text-align: center;">
-                
-                <a class="col-sm-6" style="font-size: 85px;padding: 0px 0px;margin-left: 10px;">
-                  <?php
-                  echo $row['numbers'];
-
-                  ?>
-                </a>
-                <div class="col-sm-5" style="border-left: 2px solid rgb(75, 74, 74);height: 130px;margin-top: 10px;align-content: center;">
-                  <img class="col-sm-12" src="images/pin.png">
+              <a onclick="openclose()">
+                <div class="myappoinment col-sm-12" >
+                  <span class="col-sm-6" style="font-size: 85px;padding: 0px 0px;margin-left: 10px;color:black;">
+                    <?php
+                    echo $row['numbers'];
+                    ?>
+                  </span>
+                  <div class="col-sm-5" style="border-left: 2px solid rgb(75, 74, 74);height: 130px;margin-top: 10px;align-content: center;">
+                    <img class="col-sm-12" src="images/pin.png">
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
-            <span style="font-size: 30px;color: black;">Updates</span>
+            <span class="col-sm-12" style="font-size: 30px;color: black;">Updates</span>
             <div class="col-sm-12" style="max-width:100%;background-color:whitesmoke;border-radius: 20px;padding: 10px 10px;box-shadow: 5px 5px 10px grey;margin-top: 15px;text-align: center;">
               <a class="col-sm-3">Project Name</a>
-              <a class="col-sm-3">Update</a>
-              <a class="col-sm-3">Date</a>
-              <a class="col-sm-3">Remarks</a>
+              <a class="col-sm-3" style="padding-left: 100px;">Scope</a>
+              <a class="col-sm-3" style="padding-left: 100px;">Date</a>
+              <a class="col-sm-3" style="padding-left: 60px;">Remarks</a>
               
             </div>
-            <div class="col-sm-12" style="max-width:100%;height: 700px;background-color:whitesmoke;border-radius: 20px;padding: 10px 10px;box-shadow: 5px 5px 10px grey;margin-top: 15px;text-align: center;">
-            <table   class="table table-hover my-1 caption-top table-borderless">
-                  <thead class="table-dark">
-                    <tr >
-                      <th class="fw-light" ></th>
-                      <th class="fw-light" ></th>
-                      <th class="fw-light" ></th>
-                      <th class="fw-light" ></th>
-                    </tr>
-                  </thead>
+            <div class="col-sm-12" style="max-width:100%;height: 700px;background-color:whitesmoke;border-radius: 5px;padding: 10px 10px;box-shadow: 5px 5px 10px grey;margin-top: 15px;text-align: center;">
+            <table   class="table table-hover my-1 caption-bottom table-borderless" >
+                  
                       <tbody class="color">
                       <?php
                         include 'connection.php';
@@ -177,7 +211,7 @@ $row3 = mysqli_fetch_array($project_result3);
 
                             
                             $Project_Name = $row["project_name"];
-                            $Details =$row["project_info"];
+                            $scope =$row["scope"];
                             $Date = $row["Pdate"];
                             $Remarks = $row["remarks"];
                           
@@ -185,11 +219,12 @@ $row3 = mysqli_fetch_array($project_result3);
                           
                             echo 
                             
-                            "<tr>"
-                            ."<td>$Project_Name</td>"
-                            ."<td>$Details</td>"
-                            ."<td>$Date</td>"
-                            ."<td>$Remarks</td>";
+                            
+                            "<td >$Project_Name</td>"
+                            ."<td >$scope</td>"
+                            ."<td >$Date</td>"
+                            ."<td >$Remarks</td>"
+                            ."<tr>";
                             
 
                                 }
@@ -202,8 +237,88 @@ $row3 = mysqli_fetch_array($project_result3);
                       </tbody>
                     </table>
             </div>
+            
+            <div class="AL col-sm-12" id="AL">
+              <a class="close" aria-label="Close" onclick="listclose()">
+                <span aria-hidden="true" >&times;</span>
+              </a>
+              <div class="content">
+                  <h2>List of Appointments</h2>
+                  <table   class="table table-hover my-1 caption-top table-borderless">
+                    <thead class="table-dark">
+                      <tr >
+                        <th class="fw-light">CustomerName</th>
+                        <th class="fw-light">Date Time</th>
+                        <th class="fw-light">Details</th>
+                        <th class="fw-light">Status</th>
+                        <th class="fw-light"></th>
+                      
+                        
+                      </tr>
+                    </thead>
+                        <tbody class="color">
+                                          <?php
+                            
+                            include 'connection.php';
+                          
+                          $Uname = $_SESSION['username'];
+                          
+                            $sql1 = "SELECT * FROM user WHERE username = '$Uname'"  ;
+                            $result1 = $conn->query($sql1);
+                            if ($result1->num_rows>0)
+                            {
+                                while($row=$result1->fetch_assoc())
+                                {
+                                  $uid = $row['UID'];
+                                  $Firstname1 = $row['fname'];
+                                  $Midname1 = $row['mname'];
+                                  $Lastname1 = $row['lname'];
+                                
+                                }
+                            }
+                            $sql3 = "SELECT * FROM appointment WHERE UID = '$uid' ORDER BY date ASC";
+
+                            $result = $conn->query($sql3);
+                            
+                            if($result->num_rows>0)
+                            {
+
+                              // output data of each row
+
+                            while($row=$result->fetch_assoc())
+                                {
+
+                              
+                              $Date_time = $row["date"];
+                              $Details =$row["a_details"];
+                              $Status = $row["status"];
+                            
+                              
+                            
+                              echo 
+                              
+                              "<tr>"
+                              ."<td>$Firstname1"." $Midname1"." $Lastname1</td>"
+                              ."<td>$Date_time</td>"
+                              ."<td>$Details</td>"
+                              ."<td>$Status</td>";
+                              
+
+                                  }
+                                echo "</table>";
+                              }else{
+                                  echo "0 results";}
+                              $conn->close();
+                            ?>
+
+
+                        </tbody>
+                      </table>
+              </div>
+            </div>    
           </div>
-        </div>   
+        </div>
+           
       </div>
       
       
